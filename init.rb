89f74a9e6717eadde3c7cdc1761aa3449d2bf8f7
Alexander Meindl <alex@alphanodes.com>
2019-08-07 20:32:41 +0200

    ruby 2.3 is required

--- init.rb
+++ init.rb
@@ -1,3 +1,5 @@
+raise "\n\033[31mredmine_mentions requires ruby 2.3 or newer. Please update your ruby version.\033[0m" if RUBY_VERSION < '2.3'
+
 require_dependency 'redmine_mentions/hooks'
 
 Rails.configuration.to_prepare do
@@ -8,7 +10,7 @@ Redmine::Plugin.register :redmine_mentions do
   name 'Redmine Mentions'
   author 'Arkhitech'
   description 'This is a plugin for Redmine which gives suggestions on using username in comments'
-  version '0.0.2'
+  version '0.0.3'
   url 'https://github.com/arkhitech/redmine_mentions'
   author_url 'http://www.arkhitech.com/'
   settings default: { 'trigger' => '@' }, partial: 'settings/mention'
--- lib/redmine_mentions/journal_patch.rb
+++ lib/redmine_mentions/journal_patch.rb
@@ -5,19 +5,19 @@ module RedmineMentions
         after_create :send_mail
 
         def send_mail
-          if journalized.is_a?(Issue) && notes.present?
-            issue = journalized
-            project = journalized.project
-            users = project.users.to_a.delete_if { |u| (u.type != 'User' || u.mail.empty?) }
-            users_regex = users.collect { |u| "#{Setting.plugin_redmine_mentions['trigger']}#{u.login}" }.join('|')
-            regex_for_email = '\B(' + users_regex + ')\b'
-            regex = Regexp.new(regex_for_email)
-            mentioned_users = notes.scan(regex)
-            mentioned_users.each do |mentioned_user|
-              username = mentioned_user.first[1..-1]
-              if user = User.find_by(login: username)
-                Mailer.notify_mentioning(user, issue, self).deliver
-              end
+          return if !journalized.is_a?(Issue) || notes.blank?
+
+          issue = journalized
+          project = journalized.project
+          users = project.users.to_a.delete_if { |u| (u.type != 'User' || u.mail&.empty?) }
+          users_regex = users.collect { |u| "#{Setting.plugin_redmine_mentions['trigger']}#{u.login}" }.join('|')
+          regex_for_email = '\B(' + users_regex + ')\b'
+          regex = Regexp.new(regex_for_email)
+          mentioned_users = notes.scan(regex)
+          mentioned_users.each do |mentioned_user|
+            username = mentioned_user.first[1..-1]
+            if user = User.find_by(login: username)
+              Mailer.notify_mentioning(user, issue, self).deliver
             end
           end
         end
